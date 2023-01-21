dcf46ca379ddd46fe9e23b6794771ad0106604d8
olegtititele <96022759+olegtititele@users.noreply.github.com>
2023-01-21 03:52:50 +0300

    Update ChoosePars.cs

--- Modules/ChoosePars.cs
+++ Modules/ChoosePars.cs
@@ -33,22 +33,22 @@ namespace Modules
 
             while(true)
             {
-                // if(ProjectFunctions.Functions.CheckSubChannel(botClient.GetChatMemberAsync(Config.workChatId, chatId).Result.Status.ToString()))
-                // {
-                //     DB.UpdateParser(chatId, "Stop");
+                if(ProjectFunctions.Functions.CheckSubChannel(botClient.GetChatMemberAsync(Config.workChatId, chatId).Result.Status.ToString()))
+                {
+                    DB.UpdateParser(chatId, "Stop");
 
-                //     using (var fileStream = new FileStream(mainMenuPhoto, FileMode.Open, FileAccess.Read, FileShare.Read))
-                //     {
-                //         await botClient.SendPhotoAsync(
-                //             chatId: chatId,
-                //             photo: new InputOnlineFile(fileStream),
-                //             caption: "<b>⛔️ Парсинг остановлен!</b>",
-                //             parseMode: ParseMode.Html,
-                //             replyMarkup: Keyboards.backToMenu
-                //         );
-                //     }
-                //     return;
-                // }
+                    using (var fileStream = new FileStream(mainMenuPhoto, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        await botClient.SendPhotoAsync(
+                            chatId: chatId,
+                            photo: new InputOnlineFile(fileStream),
+                            caption: "<b>⛔️ Парсинг остановлен!</b>",
+                            parseMode: ParseMode.Html,
+                            replyMarkup: Keyboards.backToMenu
+                        );
+                    }
+                    return;
+                }
 
                 if(DB.GetParser(chatId) == "Stop")
                 {
