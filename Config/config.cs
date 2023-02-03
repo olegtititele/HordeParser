9edb5ac8a2f6460bfcdfccb2878b8ecdd23c3e33
olegtititele <96022759+olegtititele@users.noreply.github.com>
2023-02-03 14:51:10 +0300

    Update config.cs

--- Config/config.cs
+++ Config/config.cs
@@ -4,7 +4,7 @@ namespace ConfigFile
 {
     public class Config
     {
-        public static string Bot_Token = "5934440175:AAHxnKKEXWOUaN0GUwdkyYmhg7UWVWPX0AQ";
+        public static string Bot_Token = "5853803990:AAEQkmE9n_YDuusHJCRagiO27BVQpOEnHxQ";
         public static string menuPhoto = "background.jpg";
         public static string errorMessage = "Произошла неизвестная ошибка. Попробуйте еще раз.";
         public static long[] adminChatsId = {5468787377};
